54e0e90f93259b13b6586a42241765b285b66ae9
lincolnzjx <lincolnz9511>
2019-09-08 22:28:19 +0800

    update

--- src/config.py
+++ src/config.py
@@ -67,8 +67,8 @@ class Config:
         self.parser.add_argument("--weight_decay", default=5e-4, type=float,
                                  help="weight decay for optimizer")
 
-        self.parser.add_argument("--use_SGD", default=True, type=str2bool,
-                                 help="use SGD or Adam")
+        #self.parser.add_argument("--use_SGD", default=True, type=str2bool,
+        #                         help="use SGD or Adam")
         self.parser.add_argument("--input_size", default=224, type=int,
                                  help="image input size for model")
         self.parser.add_argument("--backbone", default="resnet34", type=str,
@@ -80,40 +80,41 @@ class Config:
     def _add_customized_setting(self):
         """Add customized setting
         """
-        self.parser.add_argument('--embedding_len', default=128, type=int,
-                                 help='128, 256, 512, 1024, 2048')
-        self.parser.add_argument('--normalize', default=True, type=str2bool,
-                                 help='128, 256, 512, 1024, 2048')
-        self.parser.add_argument('--margin', default=0.2, type=float,
-                                 help='margin for triplet loss')
-        self.parser.add_argument('--K', default=3, type=int,
-                                 help="postive dot for training triplets")
-        self.parser.add_argument('--M', default=3, type=int,
-                                 help='negative dot for testing triplets')
-        self.parser.add_argument('--k_predict', default=3, type=int,
-                                 help='k used for predict phase')
-        self.parser.add_argument('--batch_n_classes', default=10, type=int,
-                                 help='depend on your dataset')
-        self.parser.add_argument('--batch_n_class_num', default=20, type=int,
-                                 help='depend on your dataset, \
-                                 number for each class per batch')
-        self.parser.add_argument("--metric_function", default="l2", type=str,\
-                                 help="metric function used for \
-                                 pairwise_distance, l2 or cos")
-        self.parser.add_argument("--server", default="local", type=str,
-                                 help="server to run the code")
-        self.parser.add_argument("--bilinear", default=True, type=str2bool,
-                                 help="use bilinear within model")
-        self.parser.add_argument("--finetune", default="", type=str,
-                                 help=" \"\" or 2001\"40")
-        self.parser.add_argument("--freeze", default=True, type=str2bool,
-                                 help="freeze embedding parameter when ")
-        self.parser.add_argument("--save_memory", default=False, type=str2bool,
-                                 help="Save memory when calculate pairdistance ")
-        self.parser.add_argument("--triplet_method", default="batch_all",
-                                 type=str, help="batch_all, batch_hard")
-        self.parser.add_argument("--use_sampler", default=False,
-                                 type=str2bool, help="False or True")
+        pass
+        #self.parser.add_argument('--embedding_len', default=128, type=int,
+        #                         help='128, 256, 512, 1024, 2048')
+        #self.parser.add_argument('--normalize', default=True, type=str2bool,
+        #                         help='128, 256, 512, 1024, 2048')
+        #self.parser.add_argument('--margin', default=0.2, type=float,
+        #                         help='margin for triplet loss')
+        #self.parser.add_argument('--K', default=3, type=int,
+        #                         help="postive dot for training triplets")
+        #self.parser.add_argument('--M', default=3, type=int,
+        #                         help='negative dot for testing triplets')
+        #self.parser.add_argument('--k_predict', default=3, type=int,
+        #                         help='k used for predict phase')
+        #self.parser.add_argument('--batch_n_classes', default=10, type=int,
+        #                         help='depend on your dataset')
+        #self.parser.add_argument('--batch_n_class_num', default=20, type=int,
+        #                         help='depend on your dataset, \
+        #                         number for each class per batch')
+        #self.parser.add_argument("--metric_function", default="l2", type=str,\
+        #                         help="metric function used for \
+        #                         pairwise_distance, l2 or cos")
+        #self.parser.add_argument("--server", default="local", type=str,
+        #                         help="server to run the code")
+        #self.parser.add_argument("--bilinear", default=True, type=str2bool,
+        #                         help="use bilinear within model")
+        #self.parser.add_argument("--finetune", default="", type=str,
+        #                         help=" \"\" or 2001\"40")
+        #self.parser.add_argument("--freeze", default=True, type=str2bool,
+        #                         help="freeze embedding parameter when ")
+        #self.parser.add_argument("--save_memory", default=False, type=str2bool,
+        #                         help="Save memory when calculate pairdistance ")
+        #self.parser.add_argument("--triplet_method", default="batch_all",
+        #                         type=str, help="batch_all, batch_hard")
+        #self.parser.add_argument("--use_sampler", default=False,
+        #                         type=str2bool, help="False or True")
 
     def _load_common_setting(self):
         """Load default setting from Parser
@@ -139,44 +140,43 @@ class Config:
         self.config["eps"] = self.args.eps
         self.config["weight_decay"] = self.args.weight_decay
 
-        self.config["use_SGD"] = self.args.use_SGD
         self.config["input_size"] = self.args.input_size
         self.config["backbone"] = self.args.backbone
         self.config["re_size"] = self.args.re_size
-        self.config["use_sampler"] = self.args.use_sampler
 
     def _load_customized_setting(self):
         """Load sepcial setting
         """
-        self.config['embedding_len'] = self.args.embedding_len
-        self.config["normalize"] = self.args.normalize
-        self.config['margin'] = self.args.margin
-        self.config["M"] = self.args.M
-        self.config["K"] = self.args.K
-        self.config["k_predict"] = self.args.k_predict
-        self.config['batch_n_class_num'] = self.args.batch_n_class_num
-        self.config['batch_n_classes'] = self.args.batch_n_classes
-        self.config["metric_function"] = self.args.metric_function
-        self.config["server"] = self.args.server
-        self.config["bilinear"] = self.args.bilinear
-        self.config["finetune"] = self.args.finetune
-        self.config["freeze"] = self.args.freeze
-        self.config["save_memory"] = self.args.save_memory
-        self.config["triplet_method"] = self.args.triplet_method
+        #self.config['embedding_len'] = self.args.embedding_len
+        #self.config["normalize"] = self.args.normalize
+        #self.config['margin'] = self.args.margin
+        #self.config["M"] = self.args.M
+        #self.config["K"] = self.args.K
+        #self.config["k_predict"] = self.args.k_predict
+        #self.config['batch_n_class_num'] = self.args.batch_n_class_num
+        #self.config['batch_n_classes'] = self.args.batch_n_classes
+        #self.config["metric_function"] = self.args.metric_function
+        #self.config["server"] = self.args.server
+        #self.config["bilinear"] = self.args.bilinear
+        #self.config["finetune"] = self.args.finetune
+        #self.config["freeze"] = self.args.freeze
+        #self.config["save_memory"] = self.args.save_memory
+        #self.config["triplet_method"] = self.args.triplet_method
+        pass
 
-    def _modify_config(self):
-        """Modify some config
-        """
-        # freeze only used when finetune
-        if self.config["finetune"] == False:
-            self.args.freeze = False
+    #def _modify_config(self):
+    #    """Modify some config
+    #    """
+    #    # freeze only used when finetune
+    #    if self.config["finetune"] == False:
+    #        self.args.freeze = False
 
     def _path_suitable_for_server(self):
         """Path suitable for server
         """
         if self.config["server"] == "local":
-            self.config["log_dir"] = "/media/lincolnzjx/Disk2/saved/logdirs"
-            self.config["model_dir"] = "/media/lincolnzjx/Disk2/saved/models"
+            self.config["log_dir"] = "/media/lincolnzjx/Disk2/interpretation/saved/logdirs"
+            self.config["model_dir"] = "/media/lincolnzjx/Disk2/interpretation/saved/models"
         elif self.config["server"] == "ls15":
             self.config["log_dir"] = "/data15/jiaxin/Fine-Grained-Recognition/saved/logdirs"
             self.config["model_dir"] = "/data15/jiaxin/Fine-Grained-Recognition/saved/models"
