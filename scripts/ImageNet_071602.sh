50094a91c232e08d866ba7b2c8bc7e47e068a576
lincolnzJx <lincolnz9511@gamil.com>
2020-07-17 16:50:19 +0800

    2020/0717 udpate all

---
+++ scripts/ImageNet_071602.sh
@@ -0,0 +1,62 @@
+set -e
+set -x
+export PYTHONPATH='src'
+#log_dir="./saved/logs"
+
+# Experient index!!
+file_name=`basename $0`
+experiment_index=${file_name##*_}
+experiment_index=${experiment_index%%.*}
+dataset=ImageNet
+# -------------------------------
+class_index=950
+num_class=30
+server=ls97
+delta=0
+eval_frequency=10000
+# -------------------------------
+# Variables
+n_epochs=100000
+
+cuda_visible_devices=0
+selected_layer=29
+selected_filter=334
+alpha=0
+beta=0
+gamma=1
+guidedReLU=False
+backbone=vgg16
+seed=-1
+
+
+CUDA_VISIBLE_DEVICES=$cuda_visible_devices python -u src/trainer.py \
+    --experiment_index=$experiment_index \
+    --server=$server \
+    --cuda=0 \
+    --dataset=$dataset \
+    --n_epochs=$n_epochs \
+    --eval_frequency=$eval_frequency \
+    --re_size=224 \
+    --backbone=$backbone \
+    --optimizer=SGD \
+    --learning_rate=1e-1 \
+    --alpha=$alpha \
+    --gamma=$gamma \
+    --beta=$beta \
+    --delta=$delta \
+    --selected_layer=$selected_layer \
+    --selected_filter=$selected_filter \
+    --resume=037-0 \
+    --num_class=$num_class \
+    --class_index=$class_index \
+    --mode=keep \
+    --inter=False \
+    --rho=0 \
+    --regularization=L1 \
+    --smoothing=TotalVariation \
+    --regular_ex=3 \
+    --delta=$delta \
+    --rescale=False \
+    --guidedReLU $guidedReLU \
+    --seed $seed\
+    2>&1 | tee $log_file
